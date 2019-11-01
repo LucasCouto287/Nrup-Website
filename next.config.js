ec9bbdb58d8cb12378da574aaf104817a3ce8d7d
Nrup R Patel <nruppatel133@gmail.com>
2019-11-01 00:05:38 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const withImages = require("next-images");
-
-module.exports = withImages({
-  webpack(config, options) {
-    config.resolve.alias["components"] = path.join(__dirname, "components");
-    return config;
-  }
-});
