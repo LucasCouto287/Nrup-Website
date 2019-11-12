c7d7ffa482ef676d50bcc6b349b88a4e97aca624
Nrup R Patel <nruppatel133@gmail.com>
2019-11-12 02:06:38 -0500

    Update nrup.js

--- js/nrup.js
+++ js/nrup.js
@@ -14,3 +14,14 @@ window.smoothScroll = function(target) {
         if (target == scrollContainer) break;
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
+
+    targetY += 15;
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 10);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
