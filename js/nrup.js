56bdd7f57c8a7361c917488b63864c7fa95a28e3
Nrup R Patel <nruppatel133@gmail.com>
2019-11-12 02:06:08 -0500

    Create nrup.js

---
+++ js/nrup.js
@@ -0,0 +1,10 @@
+setTimeout(updateSubtitle, 1500)
+
+// Testing this
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
